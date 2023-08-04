cc89fa13a90b2b3215caa822367ae06704e7aad1
anishka22y <129929964+anishka22y@users.noreply.github.com>
2023-08-04 17:29:51 +0530

    initial commit

---
+++ leet2.py
@@ -0,0 +1,27 @@
+def roman_to_int(s):
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for char in s[::-1]:
+        value = roman_numerals[char]
+        if value >= prev_value:
+            result += value
+        else:
+            result -= value
+        prev_value = value
+
+    return result
+
+s = "III"
+output = roman_to_int(s)
+print(output)  # Output: 3
